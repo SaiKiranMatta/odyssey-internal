c3414329eab02b6bde797efa16d5a6153d05e537
SaiKiranMatta <saikirangoud.matta@gmail.com>
2024-02-20 23:02:58 +0530

    add: level 5

--- src/app/game/page.jsx
+++ src/app/game/page.jsx
@@ -4,10 +4,11 @@ import Level1 from "@/components/levels/Level1";
 import Level2 from "@/components/levels/Level2";
 import Level3 from "@/components/levels/Level3";
 import Level4 from "@/components/levels/Level4";
+import Level5 from "@/components/levels/Level5";
 import React, { useEffect, useState } from "react";
 
 const Game = () => {
-    const [currentLevel, setCurrentLevel] = useState(4);
+    const [currentLevel, setCurrentLevel] = useState(5);
 
     const handleLevelComplete = (curLevel) => {
         console.log("function called");
@@ -21,6 +22,7 @@ const Game = () => {
             {currentLevel === 2 && <Level2 onComplete={handleLevelComplete} />}
             {currentLevel === 3 && <Level3 onComplete={handleLevelComplete} />}
             {currentLevel === 4 && <Level4 onComplete={handleLevelComplete} />}
+            {currentLevel === 5 && <Level5 onComplete={handleLevelComplete} />}
         </div>
     );
 };
